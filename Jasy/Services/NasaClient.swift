5f9ba849a452b85ee6d43c1c0dc38ae923d6bf18
Vladimir Espinola <vladimir@Vladimirs-Mac-mini.local>
2018-09-14 23:05:31 -0400

    created first class

---
+++ Jasy/Services/NasaClient.swift
@@ -0,0 +1,15 @@
+//
+//  NasaClient.swift
+//  Jasy
+//
+//  Created by Vladimir Espinola on 9/14/18.
+//  Copyright © 2018 Vladimir Espinola. All rights reserved.
+//
+
+import UIKit
+
+class NasaClient: NSObject {
+    static func request() {
+        
+    }
+}
